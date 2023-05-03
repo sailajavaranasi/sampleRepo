99de28a26545f89104e69f173f2a7dcfac625173
sailajavaranasi <svaranasi.qa@gmail.com>
2023-05-02 18:28:08 -0700

    Updated New Class for the 3rd time

--- sample project1/src/sample/package1/NewClass.java
+++ sample project1/src/sample/package1/NewClass.java
@@ -6,6 +6,7 @@ public class NewClass {
 		// TODO Auto-generated method stub
 		System.out.println("New Class Created");
 		System.out.println("Update New Class");
+		System.out.println("3rd Updated New Class");
 
 
 	}
