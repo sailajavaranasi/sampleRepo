912f0115c8016c75e114688b44ca412fd4e64fc1
sailajavaranasi <svaranasi.qa@gmail.com>
2023-05-02 18:08:58 -0700

    Added New Class to Sample Project1

---
+++ sample project1/src/sample/package1/NewClass.java
@@ -0,0 +1,11 @@
+package sample.package1;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Class Created");
+
+	}
+
+}
