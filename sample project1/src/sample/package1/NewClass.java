3ef25f898e60b5c60b6a00e9b7c03aed0ebd87a5
sailajavaranasi <svaranasi.qa@gmail.com>
2023-05-02 18:25:33 -0700

    Updated New Class

--- sample project1/src/sample/package1/NewClass.java
+++ sample project1/src/sample/package1/NewClass.java
@@ -5,6 +5,8 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New Class Created");
+		System.out.println("Update New Class");
+
 
 	}
 
